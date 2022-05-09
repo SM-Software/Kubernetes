33e91453dbd1d07f32c4436e42c4d208e9361829
SM-Software <software.sm@outlook.com>
2022-05-09 22:09:18 +0530

    Added the files to deploy the acr & aks with terraform

---
+++ Applications/Deploy-mediawiki-to-AKS/ACR and AKS - Terraform/2. variables.tf
@@ -0,0 +1,48 @@
+variable "appname"{
+  default = "mediawiki"
+  type= string
+}
+
+variable "location" {
+  default = "east us 2"
+  description = "Geographical location"
+  type = string
+}
+
+variable "resource_group_name"{
+    default= "MediaWiki-Aks-RG"
+    type= string
+}
+
+variable "acr_name"{
+    default = "mediawikiACR"
+    type= string
+}
+
+variable "default_aks_node_count" {
+  default = 2
+  type= number
+}
+
+variable "aks_node_vm_size" {
+  default = "Standard_D2_V2"
+}
+
+variable "azure_aks_aad_group_name"{
+  default = "aks-admins"
+}
+
+variable "windows_admin_username"{
+  default = "IamAdmin"
+  type = string
+}
+
+variable "windows_admin_password"{
+  default = "VirtualMachine@123"
+  type = string
+}
+
+variable "ssh_public_key"{
+  default = "C:\\Users\\v-saummi\\Code Folder\\Kubernetes\\Applications\\Deploy-mediawiki-to-AKS\\ACR and AKS - Terraform\\ssh-keys.pub"
+  type=string
+}
---
+++ Applications/Deploy-mediawiki-to-AKS/ACR and AKS - Terraform/readme.md
@@ -0,0 +1,4 @@
+Run command to generate the ssh files
+```
+ssh-keygen -m PEM -t rsa -b 4096 -C "email" -f ./ssh-keys -N <passowrd>
+```
